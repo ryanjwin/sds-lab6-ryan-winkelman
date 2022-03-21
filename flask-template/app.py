6daab05cda05dadb995b2e8a88d04cd021addc40
ryanjwin <ryanjwin2020@gmail.com>
2022-03-20 21:16:54 -0600

    added render template

--- flask-template/app.py
+++ flask-template/app.py
@@ -15,8 +15,8 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
 
 
 # -- Initialization section --
@@ -26,5 +26,5 @@ app = Flask(__name__)
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
-def index():
-    return "hello world"
+def index(name = None):
+    return render_template('index.html', name = name)
