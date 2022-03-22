523080c7f8f1c76795d859a032a7390807391a6b
ryanjwin <ryanjwin2020@gmail.com>
2022-03-21 21:33:12 -0600

    added names

--- flask-template/app.py
+++ flask-template/app.py
@@ -12,6 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+# Lab 6 Partners:
+
+# Ryan Winkelman
+# Hiram Lopez
+
+
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
